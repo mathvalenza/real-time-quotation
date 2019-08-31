1aa0953d2c5ccd15ca9111762c9edae1c73d1928
Matheus Valenza <matheusvvalenza@gmail.com>
2019-08-31 15:24:29 -0300

    added first layout version to login n and app bar

---
+++ src/components/CustomAppBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar absolute class="primary white--text">
+    <v-toolbar-title>Real Time Quotation</v-toolbar-title>
+    <div class="flex-grow-1"></div>
+    <v-btn class="white--text" icon :disabled="isLoginView" @click="goToLogin">
+      <v-icon>mdi-logout</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'CustomAppBar',
+  computed: {
+    isLoginView() {
+      return this.$route.name === 'login';
+    },
+  },
+  methods: {
+    goToLogin() {
+      return this.$router.push({ name: 'login' });
+    },
+  },
+};
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container fluid fill-height>
+    <v-row justify="center">
+      <v-col lg="6" md="8" sm="6">
+        <v-form>
+          <v-text-field
+            clearable
+            v-model="form.email"
+            label="Login"
+            :rules="[rules.required]"
+          />
+          <v-text-field
+            v-model="form.password"
+            label="Senha"
+            name="input-10-1"
+            :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+            :rules="[rules.required, rules.min]"
+            :type="showPassword ? 'text' : 'password'"
+            @click:append="showPassword = !showPassword"
+          />
+          <v-row justify="end">
+            <v-btn @click="login">Entrar</v-btn>
+          </v-row>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      showPassword: false,
+      form: {
+        email: '',
+        password: '',
+      },
+      rules: {
+        required: v => !!v || 'Este campo é obrigatório.',
+        min: v => (v && v.length >= 6) || 'A senha deve possuir no mínimo 6 caracteres',
+      },
+    };
+  },
+  methods: {
+    login() {
+      this.$router.push({ name: 'home' });
+    },
+  },
+};
+</script>
